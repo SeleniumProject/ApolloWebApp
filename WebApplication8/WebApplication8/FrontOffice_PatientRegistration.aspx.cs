505ec07b939affd62babe65e4e5303b79f88fb26
Ramesh Kudikala <Ramesh.Kudikala@ap.jll.com>
2022-10-24 16:46:36 +0530

    Changes commit with New feature Patient Registration

---
+++ WebApplication8/WebApplication8/FrontOffice_PatientRegistration.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication8
+{
+    public partial class FrontOffice_PatientRegistration : System.Web.UI.Page
+    {
+        int randomvalue;
+        public int GetRandomNum()
+        {
+
+            Random rnd = new Random();
+
+            for (int j = 0; j < 2; j++)
+            {
+                randomvalue = rnd.Next();
+                Console.WriteLine(rnd.Next());
+            }
+            return randomvalue;
+
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox2.Text = "AP"+GetRandomNum().ToString();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            successmsg.Text = "You have successfully Registered : ID: " + TextBox2.Text;
+            TextBox3.Text = "";
+            TextBox5.Text = "";
+            TextBox6.Text = "";
+            TextBox7.Text = "";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox3.Text = "";
+            TextBox5.Text = "";
+            TextBox6.Text = "";
+            TextBox7.Text = "";
+        }
+    }
+}
